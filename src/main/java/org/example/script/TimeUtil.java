6749dc8b3639cb0307645853eb8d75d2771a5ef1
mmlhl <70283915+mmlhl@users.noreply.github.com>
2025-03-21 00:05:02 +0800

    增加一个工具类示例，后续这个工具内方法将会被打包进入main.java

---
+++ src/main/java/org/example/script/TimeUtil.java
@@ -0,0 +1,18 @@
+package org.example.script;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimeUtil {
+    public static String getCurrentDate() {
+        Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return dateFormat.format(date);
+    }
+
+    public static String getCustomFormat(String pattern) {
+        Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        return dateFormat.format(date);
+    }
+}
